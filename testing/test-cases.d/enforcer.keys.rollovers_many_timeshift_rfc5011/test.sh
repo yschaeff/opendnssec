18f56938b3f9ba08eabe001b2cdc7d1c1739de7c
warren-conac <wanggd@conac.cn>
2014-10-31 10:24:40 +0800

    Add check KSK sign to the test script

--- testing/test-cases.d/enforcer.keys.rollovers_many_timeshift_rfc5011/test.sh
+++ testing/test-cases.d/enforcer.keys.rollovers_many_timeshift_rfc5011/test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #
-#TEST: Test to track KSK(5011) rollovers in real time from the enforcer side only. 
+#TEST: Test to track KSK(5011) rollovers in real time from the enforcer side only.
+#TEST: Test the retire and dead ksk does not sign
 #TEST: Configured with 1Y key lifetimes and 1 min enforcer interval.
 #TEST: unlike parent test this uses TIMESHIFT to hopefully keep things deterministic
 #TEST: Checks the output of ods-ksmutil key list and the signconf.xml contents
@@ -11,29 +12,17 @@
 
 ENFORCER_WAIT=90	# Seconds we wait for enforcer to run
 
-cp_signconfs_at_timestep_Y () {
-
-	for zone in 1 2; do
-        # Used only to create a gold while setting up the test
-        #cp $INSTALL_ROOT/var/opendnssec/signconf/ods$zone.xml gold/ods_signconf_ods"$zone"_"$1".xml        
-        cp $INSTALL_ROOT/var/opendnssec/signconf/ods$zone.xml base/ods_signconf_ods"$zone"_"$1".xml 		
-	done	
-
-}
-
 if [ -n "$HAVE_MYSQL" ]; then
         ods_setup_conf conf.xml conf-mysql.xml
 fi &&
 
 ods_reset_env &&
 
-rm -rf base &&
-mkdir  base &&
-
 ##################  SETUP ###########################
 # Start enforcer (Zone already exists and we let it generate keys itself)
 export ENFORCER_TIMESHIFT='01-01-2010 12:00:00' &&
-ods_start_enforcer_timeshift &&
+#ods_start_enforcer_timeshift &&
+log_this ods-ksmutil-key-list0 ods-enforcerd -1 &&
 
 # Make sure TIMESHIFT worked:
 syslog_grep "ods-enforcerd: .*Timeshift mode detected, running once only!" &&
@@ -42,6 +31,16 @@ syslog_grep "ods-enforcerd: .*DEBUG: Timeshift in operation; ENFORCER_TIMESHIFT 
 # Check that we have 1 KSK active
 log_this ods-ksmutil-key-list0 ods-ksmutil key list --keytype ksk &&
 log_grep ods-ksmutil-key-list0 stdout 'ods1                            KSK           active    2011-01-01 12:00:00' &&
+
+log_this ods-ksmutil-cka_id0 ods-ksmutil key list --all --verbose &&
+KSK_Tag_active=`log_grep -o ods-ksmutil-cka_id0 stdout "ods1                            KSK           active" | awk '{print $11}'` &&
+
+log_this ods-ksmutil-sign-list0 ods-signer start &&
+log_this ods-ksmutil-sign-list0 ods-signer update --all &&
+syslog_waitfor 60 'ods-signerd: .*\[STATS\] ods1' &&
+test -f "$INSTALL_ROOT/var/opendnssec/signed/ods1" &&
+
+$GREP -q -- "id = $KSK_Tag_active (ksk), size = 2048b" "$INSTALL_ROOT/var/opendnssec/signed/ods1" &&
 
 ## Next event is KSK(n+1) published (should be between 2011-01-31 12:00:00-interval(in conf.xml) and 2011-01-31 12:03:40 )
 ##################  STEP 1: Time = 2011-01-31 12:00:00 ###########################
@@ -52,6 +51,19 @@ ods_start_enforcer_timeshift &&
 log_this ods-ksmutil-key-list1 ods-ksmutil key list &&
 log_grep ods-ksmutil-key-list1 stdout 'ods1                            KSK           active    2011-01-01 12:00:00' &&
 log_grep ods-ksmutil-key-list1 stdout 'ods1                            KSK           publish   2011-03-02 12:03:40' &&
+
+log_this ods-ksmutil-cka_id1 ods-ksmutil key list --all --verbose &&
+KSK_Tag_publish=`log_grep -o ods-ksmutil-cka_id1 stdout "ods1                            KSK           publish" | awk '{print $11}'` &&
+KSK_Tag_active=`log_grep -o ods-ksmutil-cka_id1 stdout "ods1                            KSK           active" | awk '{print $11}'` &&
+
+log_this ods-ksmutil-sign-list1 ods-signer start &&
+log_this ods-ksmutil-sign-list1 ods-signer update --all &&
+echo "sleep 10 second" &&
+sleep 10 &&
+
+#The publish ksk sign
+$GREP -q -- "id = $KSK_Tag_publish (ksk), size = 2048b" "$INSTALL_ROOT/var/opendnssec/signed/ods1" &&
+$GREP -q -- "id = $KSK_Tag_active (ksk), size = 2048b" "$INSTALL_ROOT/var/opendnssec/signed/ods1" &&
 
 ## Next event is we set time = 2011-01-31 12:03:40, the KSK will still in active, wait until KSK(n+1) to active)
 ##################  STEP 2: Time = 2011-01-31 12:00:00 ###########################
@@ -74,6 +86,19 @@ log_this ods-ksmutil-key-list3 ods-ksmutil key list &&
 log_grep ods-ksmutil-key-list3 stdout 'ods1                            KSK           retire    2011-04-01 12:08:50' &&
 log_grep ods-ksmutil-key-list3 stdout 'ods1                            KSK           active    2012-03-01 12:03:40' &&
 
+log_this ods-ksmutil-cka_id3 ods-ksmutil key list --all --verbose &&
+KSK_Tag_retire=`log_grep -o ods-ksmutil-cka_id3 stdout "ods1                            KSK           retire" | awk '{print $11}'` &&
+KSK_Tag_active=`log_grep -o ods-ksmutil-cka_id3 stdout "ods1                            KSK           active" | awk '{print $11}'` &&
+
+log_this ods-ksmutil-sign-list3 ods-signer start &&
+log_this ods-ksmutil-sign-list3 ods-signer update --all &&
+
+echo "sleep 10 second" &&
+sleep 10 &&
+# The retired ksk does not sign
+! $GREP -q -- "id = $KSK_Tag_retire (ksk), size = 2048b" "$INSTALL_ROOT/var/opendnssec/signed/ods1" &&
+$GREP -q -- "id = $KSK_Tag_active (ksk), size = 2048b" "$INSTALL_ROOT/var/opendnssec/signed/ods1" &&
+
 ## Next event is old KSK change to dead)
 ##################  STEP 4: Time = 2011-04-01 12:08:50 ###########################
 export ENFORCER_TIMESHIFT='01-04-2011 12:08:50' &&
@@ -83,6 +108,21 @@ ods_start_enforcer_timeshift &&
 log_this ods-ksmutil-key-list4 ods-ksmutil key list --all &&
 log_grep ods-ksmutil-key-list4 stdout 'ods1                            KSK           dead      to be deleted' &&
 log_grep ods-ksmutil-key-list4 stdout 'ods1                            KSK           active    2012-03-01 12:03:40' &&
+
+log_this ods-ksmutil-cka_id4 ods-ksmutil key list --all --verbose &&
+KSK_Tag_active = ""
+KSK_Tag_dead=`log_grep -o ods-ksmutil-cka_id4 stdout "ods1                            KSK           dead" | awk '{print $12}'` &&
+KSK_Tag_active=`log_grep -o ods-ksmutil-cka_id4 stdout "ods1                            KSK           active" | awk '{print $11}'` &&
+
+log_this ods-ksmutil-sign-list4 ods-signer start &&
+log_this ods-ksmutil-sign-list4 ods-signer update --all &&
+echo "sleep 10 second" &&
+sleep 10 &&
+
+# The dead ksk does not sign
+! $GREP -q -- "id = $KSK_Tag_dead (ksk), size = 2048b" "$INSTALL_ROOT/var/opendnssec/signed/ods1" &&
+$GREP -q -- "id = $KSK_Tag_active (ksk), size = 2048b" "$INSTALL_ROOT/var/opendnssec/signed/ods1" &&
+
 
 ## Next event is old KSK deleted)
 ##################  STEP 5: Time = 2011-04-01 12:08:50 ###########################
@@ -94,7 +134,6 @@ log_this ods-ksmutil-key-list5 ods-ksmutil key list --all &&
 ! log_grep ods-ksmutil-key-list5 stdout 'ods1                            KSK           dead      to be deleted' &&
 log_grep ods-ksmutil-key-list5 stdout 'ods1                            KSK           active    2012-03-01 12:03:40' &&
 
-rm -rf base &&
 
 echo &&
 echo "************OK******************" &&
